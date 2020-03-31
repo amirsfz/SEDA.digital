c460fecb5e9b29f4c60764663050eadf16d0731a
Romain Tripault <romain@melting-media.com>
2020-03-31 12:05:18 +0200

    Attempt at having tideways tracer working

--- src/Tracer/Tideways.php
+++ src/Tracer/Tideways.php
@@ -4,7 +4,58 @@ declare(strict_types=1);
 
 namespace SEDA\Tracing\Tracer;
 
-abstract class Tideways implements TracerInterface
+final class Tideways implements TracerInterface
 {
+    public function configure(): TracerInterface
+    {
+        // TODO: Implement configure() method.
+        return $this;
+    }
 
+    public function setTransactionName(string $name): TracerInterface
+    {
+        if (!$this->hasProfiler()) {
+            return $this;
+        }
+        \Tideways\Profiler::setTransactionName($name);
+
+        return $this;
+    }
+
+    public function setServiceName(string $name): TracerInterface
+    {
+        if (!$this->hasProfiler()) {
+            return $this;
+        }
+        \Tideways\Profiler::setServiceName($name);
+
+        return $this;
+    }
+
+    public function setTags(array $tags): TracerInterface
+    {
+        if (!$this->hasProfiler()) {
+            return $this;
+        }
+
+        foreach ($tags as $key => $variable) {
+            \Tideways\Profiler::setCustomVariable($key, $variable);
+        }
+
+        return $this;
+    }
+
+    public function ignoreTransaction(): void
+    {
+        if (!$this->hasProfiler()) {
+            return;
+        }
+
+        \Tideways\Profiler::ignoreTransaction();
+    }
+
+    private function hasProfiler() : bool
+    {
+        return class_exists('\Tideways\Profiler');
+    }
 }
